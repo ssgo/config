dc5cbdbf83e75457fe984bd506345355f1f478ec
Star <star3s@126.com>
2019-03-29 19:01:41 +0800

    first

---
+++ Config_test.go
@@ -0,0 +1,45 @@
+package config
+
+import (
+	"testing"
+)
+
+func TestForMap(t *testing.T) {
+	testConf := map[string]interface{}{}
+	err := LoadConfig("test", &testConf)
+	if err != nil {
+		t.Error("read test.json failed", err)
+	}
+	if testConf["name"] != "test-config" {
+		t.Error("name in test.json failed", testConf["name"])
+	}
+}
+
+type testConfType struct {
+	Name string
+	Sets []int
+	List map[string]*struct {
+		Name string
+	}
+	List2 map[string]string
+}
+
+func TestForStruct(t *testing.T) {
+	testConf := testConfType{}
+	err := LoadConfig("test", &testConf)
+	if err != nil {
+		t.Error("read test.json failed", err)
+	}
+	if testConf.Name != "test-config" {
+		t.Error("name in test.json failed", testConf.Name)
+	}
+	if len(testConf.Sets) != 3 || testConf.Sets[1] != 2 {
+		t.Error("sets in test.json failed", testConf.Sets)
+	}
+	if testConf.List != nil && testConf.List["aaa"].Name != "222" {
+		t.Error("map in test.json failed", testConf.List["aaa"])
+	}
+	if testConf.List != nil && (testConf.List["bbb"] == nil || testConf.List["bbb"].Name != "xxx") {
+		t.Error("map in env.json failed", testConf.List)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ssgo/config
+
+go 1.12
+
+require github.com/ssgo/log v0.0.2
